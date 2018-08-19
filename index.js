0f3c19f4f1fd7dba9cdb0e5ab9461339f5cb9be6
Jane Venter <janeanneventer@gmail.com>
2018-08-19 17:00:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return 'Happy holidays ${name}!'
 }
-function happyHolidayTo
+function happyHolidayTo(holiday, name){
+  return 'Happy ${holiday}, ${name}!'
+}
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`
+}
